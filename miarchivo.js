84644651efd072d0e16e8e4d324e2e0acd2ebff0
Facu Lafflitto <facundolafflitto@yahoo.com.ar>
2023-08-03 23:30:17 -0300

    eventsandstorage

--- miarchivo.js
+++ miarchivo.js
@@ -1,50 +1,81 @@
 //Calculadora Javascript
-alert("Bienvenidos a la calculadora!")
+//alert("Bienvenidos a la calculadora!")
 
 // Calculadora
 
-let nombre = prompt("Por favor ingresu su nombre")
-let edad = parseInt(prompt(" Por favor ingrese su edad "))
+//let nombre = prompt("Por favor ingresu su nombre")
+//let edad = parseInt(prompt(" Por favor ingrese su edad "))
 
-const datos = [nombre, edad]
+//const datos = [nombre, edad]
 
-alert(" BIENVENIDO/A " + nombre )
+//alert(" BIENVENIDO/A " + nombre )
 
-console.log(datos)
- 
-function Calculadora(primerNumero, segundoNumero, tercerNumero, operacion){
-    switch(operacion){
-        case '+':
-            return (primerNumero) + (segundoNumero) + (tercerNumero)
-        case '-':
-            return primerNumero - segundoNumero - tercerNumero
-        case '*':
-            return primerNumero * segundoNumero * tercerNumero       
-        case '/':
-            return (primerNumero) / (segundoNumero) / (tercerNumero) 
-        default:
-            return 0         
-    }   
-}    
+//console.log(datos)
 
-let numero1 = parseInt(prompt(" Ingrese el primer numero "))
-let numero2 = parseInt(prompt(" Ingrese el segundo numero "))
-let numero3 = parseInt(prompt(" Ingrese el tercer numero"))
-let operacion = prompt( "Ingrese la Operacion")
+//function Calculadora(primerNumero, segundoNumero, tercerNumero, operacion){
+//    switch(operacion){
+//        case '+':
+//            return (primerNumero) + (segundoNumero) + (tercerNumero)
+//        case '-':
+//            return primerNumero - segundoNumero - tercerNumero
+//        case '*':
+//            return primerNumero * segundoNumero * tercerNumero       
+//       case '/':
+//            return (primerNumero) / (segundoNumero) / (tercerNumero) 
+//        default:
+//            return 0         
+//    }   
+//}    
 
-let resultado = Calculadora (numero1, numero2, numero3, operacion)
+//let numero1 = parseInt(prompt(" Ingrese el primer numero "))
+//let numero2 = parseInt(prompt(" Ingrese el segundo numero "))
+//let numero3 = parseInt(prompt(" Ingrese el tercer numero"))
+//let operacion = prompt( "Ingrese la Operacion")
 
-alert(resultado)
+//alert(resultado)
 
-if( resultado < 100) {
-    alert("El precio es menor a 100")
-} else if (resultado < 50) {
-    alert(" El precio es menor a 50")
-} else if (resultado <= 100) {
-    alert ( "el precio es menor o igual a 100 ")
-} else {
-    alert( "El precio es mayor a 100")
+//if( resultado < 100) {
+//    alert("El precio es menor a 100")
+//} else if (resultado < 50) {
+//    alert(" El precio es menor a 50")
+//} else if (resultado <= 100) {
+//    alert ( "el precio es menor o igual a 100 ")
+//} else {
+//    alert( "El precio es mayor a 100")
+//}
+
+const carrito = document.getElementsByClassName('resultados')
+
+addEventListener('click', respuesta)
+
+function respuesta() {
+  console.log('Respuesta al click');
 }
+
+
+
+addEventListener('click', () => {
+  console.log('Respuesta del evento');
+})
+
+const products = [
+  { id: 1, titulo: "Laser", descripcion: "Luz para eliminar imperfecciones faciales" },
+  { id: 2, titulo: 'Limpieza', descripcion: 'Limpieza Facial' },
+  { id: 3, titulo: 'Microdermoabrasión', descripcion: 'Eliminacion de celulas muertas' },
+]
+
+const guardarLocal = (clave, valor) => {
+  localStorage.setItem(clave, valor)
+}
+
+guardarLocal('listadeproductos', JSON.stringify(products))
+
+
+
+
+
+
+
 
 
 
---
+++ stock.js
@@ -0,0 +1,50 @@
+const productos =
+[
+    {
+        id: 1,
+        nombre: "Laser",
+        tipo: "Tratamiento",
+        precio: 1200,
+        cantidad:1
+    },
+
+    {
+        id: 2,
+        nombre: "Limpieza",
+        tipo: "Tratamiento",
+        precio: 1200,
+        cantidad:1
+    },
+
+    {
+        id: 3,
+        nombre: "Microdermoabrasión",
+        tipo: "Tratamiento",
+        precio: 1200,
+        cantidad:1
+    },
+
+    {
+        id: 4,
+        nombre: "terapia de luz",
+        tipo: "Tratamiento",
+        precio: 1200,
+        cantidad:1
+    },
+
+    {
+        id: 5,
+        nombre: "Rellenos dérmicos",
+        tipo: "Tratamiento",
+        precio: 1200,
+        cantidad:1
+    },
+
+    {
+        id: 6,
+        nombre: "Rellenos dérmicos",
+        tipo: "Tratamiento",
+        precio: 1200,
+        cantidad:1
+    }
+];
